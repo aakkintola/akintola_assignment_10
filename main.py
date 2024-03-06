c040388f5244c72c0d6406107dc4a25f788a29d9
Akintola <akintolamarke@gmail.com>
2024-03-06 14:10:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Akintola is a Senior Azure DevOps Engineer and I am also an accountant too")
+print("Akintola will get three A's on my next Algebra Test")
+print("Akintola will get an A on his Microeconomics Test")
+print("I will become an successful entrepreneur")
